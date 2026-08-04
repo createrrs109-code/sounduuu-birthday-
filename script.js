9023f6ed998330a5b3345c77a4b5068e0af0732e
createrrs109-code <313092690+createrrs109-code@users.noreply.github.com>
2026-08-05 01:59:20 +0530

    Add birthday website

---
+++ script.js
@@ -0,0 +1,9 @@
+document
+.getElementById("startBtn")
+.addEventListener("click",()=>{
+
+    alert(
+        "Happy Birthday Sounduuu 🫶💝\n\nThis is just the beginning of your surprise ❤️"
+    );
+
+});
